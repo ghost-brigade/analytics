ac25b574184fe82ea78a76762c5d9ff735bedae3
Anthony ARJONA <anthony.arjona@outlook.com>
2023-07-13 22:54:19 +0200

    feat: add event type

---
+++ wa-api/src/events/types/Event.ts
@@ -0,0 +1,10 @@
+export enum EventType {
+  PageView = "pageview",
+  Click = "click",
+  FormSubmit = "formsubmit",
+  Custom = "custom",
+  MouseEvent = "mouseevent",
+  PageLeave = "pageleave",
+  LinkClick = "linkclick",
+  ServerEvent = "serverevent",
+}
