ed0a7beb41056318d908b28da6f4ef3f4d9603af
MoulinLouis <moulin.louis93@gmail.com>
2023-07-16 20:14:07 +0200

    feat: add EventGuard on create/create many on event controller

--- wa-api/src/events/events.controller.ts
+++ wa-api/src/events/events.controller.ts
@@ -1,17 +1,28 @@
-import { Controller, Get, Post, Body, Param, Sse } from "@nestjs/common";
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Sse,
+  UseGuards,
+} from "@nestjs/common";
 import { EventsService } from "./events.service";
 import { CreateEventDto } from "./dto/create-event.dto";
 import { Observable } from "rxjs";
+import { EventGuard } from "./event.guard";
 
 @Controller("events")
 export class EventsController {
   constructor(private readonly eventsService: EventsService) {}
 
+  @UseGuards(EventGuard)
   @Post("")
   async create(@Body() createEventDto: CreateEventDto) {
     return await this.eventsService.create(createEventDto);
   }
 
+  @UseGuards(EventGuard)
   @Post("many")
   async createMany(@Body() createEventDto: CreateEventDto[]) {
     return await this.eventsService.createMany(createEventDto);
